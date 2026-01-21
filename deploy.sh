8602d6971ac676d656a30020cbce466f20f5db91
Rakshit Hooda <rakshit.official703@gmail.com>
2026-01-21 13:59:07 +0530

    chore: add deployment script for prod setup

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# Deployment Script for MannKiBaat
+
+# Install dependencies
+npm install
+
+# Start the application
+npm start
+
+# For production deployment, you might want to use:
+# npm install -g pm2
+# pm2 start server.js --name mannkibaat
+
+echo 'Server starting on http://localhost:3003'
